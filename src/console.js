31a7ad4843e83ffacb96837a783a046bb1549996
Andrew Hammett <hammett@amh.io>
2018-01-16 17:23:47 -0800

    enable log length limit. allow for folder'd jobs

--- src/console.js
+++ src/console.js
@@ -2,7 +2,7 @@
 
 const jenkins_host = process.env.JLR_ENDPOINT || 'http://localhost:8080/jenkins'
 const allow_cidr = process.env.ALLOW_CIDR || 'x.x.x.x'
-
+const MAX_LOG_LINES = process.env.MAX_LOG_LINES || '200'
 
 module.exports.console = (event, context, callback) => {
   let found = false;
@@ -27,22 +27,43 @@ module.exports.console = (event, context, callback) => {
     return;
   }
 
-  let data = JSON.parse(event.body)
-
-  if(data.job_name && data.build_number) {
+  if(event.job_url && event.build_number) {
     const jenkinsapi  = require('jenkins-api');
     let jenkins = jenkinsapi.init(jenkins_host)
-    console.log('job: '+data.job_name + ' number: '+ data.build_number)
-    jenkins.console_output(data.job_name, data.build_number, function(err, data) {
+    let job_path = event.job_url.split('/').splice(1).join('/')
+    console.log('job: '+job_path + ' number: '+ event.build_number)
+
+    jenkins.console_output(job_path, event.build_number, function(err, data) {
       if(err) {
         console.error(err)
+        callback(null, {
+          statusCode: 200,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'empty'
+        });
         return;  
+      }
+
+      let jenkins_console_array = data.body.split('\n')
+      let jenkins_console_log
+
+      if(jenkins_console_array.length > MAX_LOG_LINES) {
+        console.log('logs reduced to ' + MAX_LOG_LINES + ' lines.')
+        jenkins_console_log = jenkins_console_array.splice(0, MAX_LOG_LINES).join('\n')
+        jenkins_console_log = [
+          'logs reduced to ' + MAX_LOG_LINES + ' lines',
+          '-----------------------',
+          ...jenkins_console_log
+        ]
+      } else {
+        console.log('logs within limit. showing full logs')
+        jenkins_console_log = jenkins_console_array.join('\n')
       }
 
       callback(null, {
         statusCode: 200,
         headers: { 'Content-Type': 'text/plain' },
-        body: data.body
+        body: jenkins_console_log
       });
     })
     return;
