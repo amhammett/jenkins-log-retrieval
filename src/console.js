2e707840b24b7d8ab1377087759980fb4da092c8
Andrew Hammett <hammett@amh.io>
2018-01-20 12:29:06 -0800

    async for maintainability.

--- src/console.js
+++ src/console.js
@@ -1,78 +1,86 @@
 'use strict';
 
-const jenkins_host = process.env.JLR_ENDPOINT || 'http://localhost:8080/jenkins'
-const allow_cidr = process.env.ALLOW_CIDR || 'x.x.x.x'
+const JENKINS_HOST  = process.env.JLR_ENDPOINT  || 'http://jenkins.url'
+const ALLOW_CIDR    = process.env.ALLOW_CIDR    || 'x.x.x.x'
 const MAX_LOG_LINES = process.env.MAX_LOG_LINES || '200'
 
-module.exports.console = (event, context, callback) => {
-  let found = false;
-  let error = false;
+const async = require('async')
+const jenkinsapi  = require('jenkins-api')
+
+let jenkins = jenkinsapi.init(JENKINS_HOST)
+
+let lookup_job_name = (event) => {
+  return event.job_url.split('/').splice(1).join('/')
+}
+
+let build_console = (event, build_data, callback) => {
+  let job_name = lookup_job_name(event)
+  let build_number = event.build_number
+
+  jenkins.console_output(job_name, build_number, function(err, data) {
+    if(err) {
+      callback(err)
+    }
+    let message = data.body.split('\r\n').splice(-MAX_LOG_LINES)
+    message.unshift(build_data, '---')
+    callback(null, message.join('\n'))
+  })
+}
+
+let build_data = (event, callback) => {
+  let job_name = lookup_job_name(event)
+  let build_number = event.build_number
+
+  jenkins.build_info(job_name, build_number, function(err, data) {
+    if(err) {
+      callback(err)
+    }
+    let build_data = data.builtOn ? 'built on ' + data.builtOn : 'probably built on master'
+    callback(null, event, build_data)
+  })
+}
+
+let restrict_access = (event, callback) => {
+  let found = false
+
   let sourceIP = event['requestContext']
     && event['requestContext']['identity']['sourceIp'] || 'local'
 
-  allow_cidr.split(' ').forEach(function(allow_mask) {
+  ALLOW_CIDR.split(' ').forEach(function(allow_mask) {
     if(sourceIP.includes(allow_mask)) {
       found = true
     }
   });
 
   if (!found && sourceIP !== 'local' && sourceIP !== 'test-invoke-source-ip') {
-    console.error('Requestor not in allow list: ' + sourceIP)
-
-    callback(null, {
-      statusCode: 403,
-      headers: { 'Content-Type': 'text/plain' },
-      body: '¯\\_(ツ)_/¯'
-    });
-    return;
+    let error_message = 'Requestor not in allow list: ' + sourceIP
+    console.error(error_message)
+    callback(error_message)
   }
 
-  if(event.job_url && event.build_number) {
-    const jenkinsapi  = require('jenkins-api');
-    let jenkins = jenkinsapi.init(jenkins_host)
-    let job_path = event.job_url.split('/').splice(1).join('/')
-    console.log('job: '+job_path + ' number: '+ event.build_number)
+  callback(null, event)
+}
 
-    jenkins.console_output(job_path, event.build_number, function(err, data) {
-      if(err) {
-        console.error(err)
-        callback(null, {
-          statusCode: 200,
-          headers: { 'Content-Type': 'text/plain' },
-          body: 'empty'
-        });
-        return;  
-      }
-
-      let jenkins_console_array = data.body.split('\n')
-      let jenkins_console_log
-
-      if(jenkins_console_array.length > MAX_LOG_LINES) {
-        console.log('logs reduced to ' + MAX_LOG_LINES + ' lines.')
-        jenkins_console_log = jenkins_console_array.splice(0, MAX_LOG_LINES).join('\n')
-        jenkins_console_log = [
-          'logs reduced to ' + MAX_LOG_LINES + ' lines',
-          '-----------------------',
-          ...jenkins_console_log
-        ]
-      } else {
-        console.log('logs within limit. showing full logs')
-        jenkins_console_log = jenkins_console_array.join('\n')
-      }
-
+module.exports.console = (event, context, callback) => {
+  async.waterfall([
+    async.apply(restrict_access, event),
+    build_data,
+    build_console
+  ], function(err, results) {
+    if(err){
+      console.error(err)
+      callback(null, {
+        statusCode: 403,
+        headers: { 'Content-Type': 'text/plain' },
+        body: '¯\\_(ツ)_/¯'
+      })
+    } else {
+      console.log(results)
       callback(null, {
         statusCode: 200,
         headers: { 'Content-Type': 'text/plain' },
-        body: jenkins_console_log
-      });
-    })
-    return;
-  }
-
-  callback(null, {
-    statusCode: 403,
-    headers: { 'Content-Type': 'text/plain' },
-    body: '¯\\_(ツ)_/¯'
-  });
-  return;
+        body: results
+      })
+    }
+  })
 }
